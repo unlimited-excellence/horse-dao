c4662565fc97cb6d28a8eee6acaa676571f140fa
likespro <likespro.eth@gmail.com>
2025-02-01 20:21:20 +0200

    Connected MongoDB to the project and created PyCharm configurations

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+if __name__ == '__main__':
+    print("Initializing database...")
+    database = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))["horse-project"]
