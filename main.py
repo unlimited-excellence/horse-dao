1d20ec178a14e90adfdd3018d6acec513e737b53
likespro <likespro.eth@gmail.com>
2025-02-05 13:07:26 +0200

    Added Changed nothing comment to code to test CodeRabbit

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from pymongo.server_api import ServerApi
 if __name__ == '__main__':
     print("Initializing database...")
     database = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))["horse-project"]
+
+    # Changed nothing
