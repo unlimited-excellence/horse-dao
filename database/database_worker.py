0ac7808861bf9cd8c37bac33ac4338cc2502dfd3
likespro <likespro.eth@gmail.com>
2025-02-08 19:54:06 +0200

    Add DatabaseWorker class and initialize MongoDB connection in main.py

---
+++ database/database_worker.py
@@ -0,0 +1,12 @@
+import os
+
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+
+class DatabaseWorker:
+    def __init__(self, mongodb_uri: str, database_name: str):
+        self.client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+        self.database = self.client[database_name]
+    def insert_one(self, collection: str, document: dict):
+        self.database[collection].insert_one(document)
