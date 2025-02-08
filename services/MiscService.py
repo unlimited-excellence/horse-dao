19367ea7175a313c2804349f677836bd82a0c4c4
likespro <likespro.eth@gmail.com>
2025-02-08 20:37:36 +0200

    Add MiscService for configuration management and initialize it and config in main.py

---
+++ services/MiscService.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from database.database_worker import DatabaseWorker
+
+
+class MiscService:
+    def __init__(self, databaseWorker: DatabaseWorker):
+        self.databaseWorker = databaseWorker
+    def get_or_create_config(self) -> dict:
+        config = self.databaseWorker.find_one('misc', {
+            "type": "config"
+        })
+        if config is None:
+            self.databaseWorker.insert_one('misc', {
+                "type": "config",
+                "createdAt": datetime.now()
+            })
+        else:
+            return config
