23c695513dab1e43a368fa630c836bda02e872ab
likespro <likespro.eth@gmail.com>
2025-03-01 17:45:02 +0000

    Added `/pay` command to Telegram Bot
    Co-authored-by: AlexJenious <AlexJenious@users.noreply.github.com>
    Co-authored-by: Nazar <nazikrachok@users.noreply.github.com>
    Co-authored-by: mrgrory <mrgrory@users.noreply.github.com>

--- endpoints/telegram.py
+++ endpoints/telegram.py
@@ -1,4 +1,5 @@
 import telebot
+import logging
 
 from time import sleep
 
@@ -12,10 +13,12 @@ class TelegramBotService:
         self.users_service = users_service
         self.notifications_service = notifications_service
         self.config = config
+        #hello AlexJenious why we are writing there, instead of that we can speak by voice chat(this is history don`t delete that)
+        #hello Nazar. I dont know why we speak there. Andrij speaks, so dont speak in voice chat
 
         @self.bot.message_handler(commands=['start'])
         def handle_start_message(message):
-            status = self.users_service.create_user(message.from_user.id, 0)
+            status = self.users_service.create_user(str(message.chat.id), 0)
             if status:
                 self.bot.send_message(message.from_user.id, "Account was created. Your ID is " + str(message.from_user.id))
             else:
@@ -23,17 +26,34 @@ class TelegramBotService:
 
             sleep(int(self.config["giveTokensWhenStartAfterSeconds"]))
             
-            users_service.give_tokens(message.chat.id, int(self.config["giveTokensWhenStartAmount"]))   
+            users_service.give_tokens(str(message.chat.id), int(self.config["giveTokensWhenStartAmount"]))   
                                       
-            self.notifications_service.send_message(message.from_user.id, "Something going on")
+            self.notifications_service.send_message(str(message.chat.id), "Something going on")
 
         @self.bot.message_handler(commands=['balance'])
         def handle_balance_message(message):
-            user_get_balance = users_service.get_balance(message.chat.id)
+            user_get_balance = users_service.get_balance(str(message.chat.id))
             if user_get_balance is None:
                 self.bot.send_message(message.from_user.id, "Please send /start")
             else:
                 self.bot.send_message(message.from_user.id, "Your balance is: " + str(user_get_balance))
-
+        @self.bot.message_handler(commands=['pay'])
+        def handle_balance_message(message):
+            args = message.text.split(' ')
+            try:
+                receiver = str(args[1])
+                amount = int(args[2])
+                if amount <= 0:
+                    self.bot.send_message(str(message.from_user.id), "Error: You can`t send negative or zero amount of tokens")
+                    return
+                if not self.users_service.is_user_registered(receiver):
+                    self.bot.send_message(str(message.from_user.id), "Error: the receiver does not exist. Double check receiver\'s user_id")
+                    return
+                result = self.users_service.transact(str(message.chat.id), receiver, amount)
+                if not result:
+                    self.bot.send_message(str(message.from_user.id), "Error: insufficient balance")
+            except:
+                self.bot.send_message(str(message.from_user.id), "Error: invalid arguments. Please call this command in the following format: /pay <to_user_id> <amount>")
+            
     def run(self):
         self.bot.infinity_polling()
--- services/UsersService.py
+++ services/UsersService.py
@@ -34,8 +34,38 @@ class UsersService:
             }
         })
         self.notifications_service.send_message(user_id, "Balance have changed by "+ str(amount) + ".\n" + "Your final balance is " + str(self.get_balance(user_id)))
+    def is_user_registered(self, user_id: str) -> bool:
+        user = self.databaseWorker.find_one('users', {
+            "userId": user_id
+        })
+        if user is None:
+            return False
+        return True
+    def transact(self, from_user : str, to_user : str, amount : int ) -> bool :
+        update_one_result = self.databaseWorker.update_one('users', {
+            "userId": from_user,
+            "balance":{
+                "$gte": amount
+            }
+        }, {
+            "$inc":{
+                "balance" : -amount
+            }
+        })
+        if update_one_result.modified_count == 0:
+            return False
+        self.databaseWorker.update_one('users', {
+            "userId" : to_user
+        }, {
+            "$inc": {
+                "balance" : amount
+            }
+        })
+        self.notifications_service.send_message(from_user,"You send " + str(amount) + " to " + to_user + ".\n" + "Now your balance is: " + str(self.get_balance(from_user)))  
+        self.notifications_service.send_message(to_user, "You received " + str(amount) + ".\n" + "From user " + str(from_user))
+        return True
         
-        #42bratuha
+        
         # user = self.databaseWorker.find_one('users', {
         #     "userId": user_id
         # })
